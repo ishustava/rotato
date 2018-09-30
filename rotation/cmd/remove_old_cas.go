4fa2c5d9b2dc66bffffddac588791098ef8b972e
Iryna Shustava <ishustava@gmail.com>
2018-09-29 17:09:45 -0700

    Add remove-old-cas command for step 3 of CA rotation

--- rotation/cmd/remove_old_cas.go
+++ rotation/cmd/remove_old_cas.go
@@ -6,10 +6,9 @@ import (
 	"code.cloudfoundry.org/credhub-cli/commands"
 	"github.com/ishustava/rotato/rotation/certificates"
 	"code.cloudfoundry.org/credhub-cli/credhub/credentials"
-	"log"
 )
 
-type RegenerateCertsCommand struct {
+type RemoveOldCAsCommand struct {
 	CredHubURL          string   `short:"u" long:"credhub-server" description:"URL of the CredHub server, e.g. https://example.com:8844" env:"CREDHUB_SERVER" required:"true"`
 	CredHubClient       string   `short:"c" long:"credhub-client" description:"UAA client for the CredHub Server" env:"CREDHUB_CLIENT" required:"true"`
 	CredHubClientSecret string   `short:"s" long:"credhub-secret" description:"UAA client secret for the CredHub Server" env:"CREDHUB_SECRET" required:"true"`
@@ -17,7 +16,7 @@ type RegenerateCertsCommand struct {
 	CaCerts             []string `long:"ca-cert" description:"Trusted CA for API and UAA TLS connections. Multiple flags may be provided." env:"CREDHUB_CA_CERT" required:"true"`
 }
 
-func (cmd RegenerateCertsCommand) Execute([]string) error {
+func (cmd RemoveOldCAsCommand) Execute([]string) error {
 	caCerts, err := commands.ReadOrGetCaCerts(cmd.CaCerts)
 	if err != nil {
 		return err
@@ -43,25 +42,21 @@ func (cmd RegenerateCertsCommand) Execute([]string) error {
 		return err
 	}
 
-	err = regenerateCerts(rootCAs, ch)
+	err = unconcatenateRootCAs(rootCAs, ch)
 	if err != nil {
 		return err
 	}
 
-	return nil
+	return regenerateCerts(rootCAs, ch)
 }
 
-func regenerateCerts(rootCAs []credentials.Certificate, credHub *credhubClient.CredHub) error {
-	for _, rootCA := range rootCAs {
-		log.Printf("Regenerating certificates for %s\n", rootCA.Name)
+func unconcatenateRootCAs(rootCAs []credentials.Certificate, credHub *credhubClient.CredHub) error {
+	for _, ca := range rootCAs {
+		ca.Value.Certificate = ca.Value.Ca
 
-		resuts, err := credHub.BulkRegenerate(rootCA.Name)
+		_, err := credHub.SetCertificate(ca.Name, ca.Value)
 		if err != nil {
 			return err
-		}
-
-		for _, regenerateResult := range resuts.Certificates {
-			log.Printf("Regenerated %s\n", regenerateResult)
 		}
 	}
 
--- rotation/cmd/rotator.go
+++ rotation/cmd/rotator.go
@@ -1,8 +1,9 @@
 package cmd
 
 type RotatorCommand struct {
-	AddNewCAs AddNewCAsCommand `command:"add-new-cas" alias:"a" description:"Regenerate new CAs and make them available to the next BOSH deploy"`
-	RegenerateCerts RegenerateCertsCommand `command:"regenerate-certs" alias:"r" description:"Regenerate new certificates"`
+	AddNewCAs AddNewCAsCommand `command:"add-new-cas" description:"Regenerate new CAs and make them available to the next BOSH deploy"`
+	RegenerateCerts RegenerateCertsCommand `command:"regenerate-certs" description:"Regenerate new certificates"`
+	RemoveOldCAs RemoveOldCAsCommand `command:"remove-old-cas" description:"Remove Old CAs"`
 }
 
 var Rotator RotatorCommand
